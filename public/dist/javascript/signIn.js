9d7c59e8cebc6f44e4a9b6648ea3a30873ed7159
Saeid Soodi <saeid.Soodi1998@gmail.com>
2024-02-27 21:46:13 +0330

    locic changed

--- public/dist/javascript/signIn.js
+++ public/dist/javascript/signIn.js
@@ -1,26 +1,27 @@
-let signInBtn =document.querySelector('.signInBtn')
-signInBtn.addEventListener('click',()=>{
+let signInBtn = document.querySelector('.signInBtn')
+signInBtn.addEventListener('click', () => {
+  let inputData = document.querySelector('.userNameOrEmail').value
+  let password = document.querySelector('.password').value
+  let userData = JSON.parse(localStorage.getItem('userData'))
+  let userLogin = localStorage.getItem('userLoggedIn')
 
-    let inputData =document.querySelector('.userNameOrEmail').value
-    let password = document.querySelector('.password').value
-    let userData = JSON.parse(localStorage.getItem('userData'))
-    let userLogin =localStorage.getItem('userLoggedIn')
+  if (userData) {
+    let userExist = userData.find(
+      (user) =>
+        user.uPassword == password.trim() &&
+        (user.uEmail == inputData.trim() || user.uUserName == inputData.trim())
+    )
 
-    if(userData){
-        let userExist =userData.find((x)=>x.uPassword==password &&(x.uEmail==inputData ||x.uUserName==inputData))
-        
-        if(inputData==''||password==''){
-            alert(`${userLogin} fill out the inputs`)
-        }
-        else if(userExist){
-            localStorage.setItem('is-Login','true');
-            alert(`You are logged in ${userLogin}`);
-            window.location.href='../Game.html'
-        }
-        else{
-            alert('Your Email or Password is incorrect')
-        }
-    }else{
-        alert('There is no user in storage with this user name ')
+    if (inputData == '' || password == '') {
+      alert(`${userLogin} fill out the inputs`)
+    } else if (userExist) {
+      localStorage.setItem('isLogin', 'true')
+      alert(`You are logged in ${userLogin}`)
+      window.location.href = './Game.html'
+    } else {
+      alert('Your Email or Password is incorrect')
     }
+  } else {
+    alert('There is no user in storage with this user name ')
+  }
 })
