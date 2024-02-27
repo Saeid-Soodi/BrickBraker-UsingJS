f584304558b50b191ab9ab771a1932c78dd5b743
Saeid Soodi <saeid.Soodi1998@gmail.com>
2024-02-27 21:50:52 +0330

    logic for sign up added

--- public/dist/javascript/Home.js
+++ public/dist/javascript/Home.js
@@ -2,3 +2,8 @@ let signInBtn = document.querySelector('.signIn')
 signInBtn.addEventListener('click',()=>{
     window.location.href="./SignIn.html"
 })
+
+let signUpBtn= document.querySelector('.signUp')
+signUpBtn.addEventListener('click',()=>{
+    window.location.href="./SignUp.html"
+})
