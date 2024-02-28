7c6f65cca01114eea80ede85a3680ca4b55a8529
Saeid Soodi <saeid.Soodi1998@gmail.com>
2024-02-28 18:04:32 +0330

    Nav component created for login logic

--- public/dist/javascript/Nav.js
+++ public/dist/javascript/Nav.js
@@ -1,5 +1,24 @@
-let isLogin= JSON.parse(localStorage.getItem('isLogin'))
+let isLogin = JSON.parse(localStorage.getItem('isLogin'))
+let navLogin = document.querySelector('.navbarLoginBtn')
+let gamePage = document.querySelector('.game')
+let whoisLogin = localStorage.getItem('userLoggedIn')
 
-if(isLogin=="true"){
-    
+navLogin.addEventListener('click', () => {
+  if (isLogin) {
+    localStorage.setItem('isLogin', 'false')
+    navLogin.textContent = 'Login'
+    window.location.href = '../public/index.html'
+  } else {
+    window.location.href = '../public/SignIn.html'
+  }
+})
+
+if (isLogin) {
+  navLogin.textContent = `${whoisLogin}`
+  navLogin.style.color = '#ff0000'
+  navLogin.style.fontWeight = 'bold'
+  navLogin.style.fontSize = '15px'
+  gamePage.style.display = 'block'
+} else {
+  gamePage.style.display = 'none'
 }
